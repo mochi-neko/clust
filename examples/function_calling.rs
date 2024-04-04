020a6bb3bd9bc17cbbe2d3ccd11694b06ea69141
mochi-neko <t.o.e.4315@gmail.com>
2024-04-04 16:10:51 +0900

    Call clust_tool via clust::attributes, add function calling descriptions

--- examples/function_calling.rs
+++ examples/function_calling.rs
@@ -4,7 +4,7 @@
 //! $ cargo run --example function_calling --features macros
 //! ```
 
-use clust::clust_macros::clust_tool;
+use clust::attributes::clust_tool;
 use clust::messages::ClaudeModel;
 use clust::messages::MaxTokens;
 use clust::messages::Message;
@@ -61,6 +61,31 @@ Here are the tools available:
 "#,
         tool_list.tools(),
     );
+    // NOTE: Expected XML format of tools are as follows:
+    // <tools>
+    //   <tool_description>
+    //     <tool_name>get_current_stock_price</tool_name>
+    //     <description>Gets the current stock price for a company. Returns float: The current stock price. Raises ValueError: if the input symbol is invalid/unknown.</description>
+    //     <parameters>
+    //       <parameter>
+    //         <name>symbol</name>
+    //         <type>String</type>
+    //         <description>The stock symbol of the company to get the price for.</description>
+    //       </parameter>
+    //     </parameters>
+    //   </tool_description>
+    //   <tool_description>
+    //     <tool_name>get_ticker_symbol</tool_name>
+    //     <description>Gets the stock ticker symbol for a company searched by name. Returns str: The ticker symbol for the company stock. Raises TickerNotFound: if no matching ticker symbol is found.</description>
+    //     <parameters>
+    //       <parameter>
+    //         <name>company_name</name>
+    //         <type>String</type>
+    //         <description>The name of the company.</description>
+    //       </parameter>
+    //     </parameters>
+    //   </tool_description>
+    // </tools>
 
     println!("{}", prompt);
 
@@ -88,6 +113,8 @@ Here are the tools available:
     let response = client
         .create_a_message(request_body.clone())
         .await?;
+    // NOTE: An example of response is as follows:
+    // "Okay, let's break this down step-by-step:\n\n<function_calls>\n<invoke>\n<tool_name>get_ticker_symbol</tool_name>\n<parameters>\n<company_name>General Motors</company_name>\n</parameters>\n</invoke>\n</function_calls>\n\nThis returns: GM\n\nGM is the stock ticker symbol for General Motors. Now let's get the current price:\n\n<function_calls>\n<invoke>\n<tool_name>get_current_stock_price</tool_name>\n<parameters>\n<symbol>GM</symbol>\n</parameters>\n</invoke>\n</function_calls>\n\nThis returns: 34.17\n\nTherefore, the current stock price of General Motors (GM) is $34.17."
 
     println!("Result:\n{}", response);
 
@@ -95,9 +122,25 @@ Here are the tools available:
     let function_calls = response
         .content
         .exclude_function_calls()?;
+    // NOTE: Extracted XML is as follows:
+    // <function_calls>
+    //   <invoke>
+    //     <tool_name>get_ticker_symbol</tool_name>
+    //     <parameters>
+    //       <company_name>General Motors</company_name>
+    //     </parameters>
+    //   </invoke>
+    // </function_calls>
 
     // 5. Call the function.
     let function_result = tool_list.call(function_calls)?;
+    // NOTE: An example of function results is as follows:
+    // <function_results>
+    //   <result>
+    //     <tool_name>get_ticker_symbol</tool_name>
+    //     <stdout>GM</stdout>
+    //   </result>
+    // </function_results>
 
     // 6. Stack a function call by assistant.
     request_body
@@ -115,6 +158,8 @@ Here are the tools available:
     let response = client
         .create_a_message(request_body.clone())
         .await?;
+    // NOTE: An example of response is as follows:
+    // "Great, now that we have the ticker symbol GM for General Motors, we can look up its current stock price:\n\n<function_calls>\n<invoke>\n<tool_name>get_current_stock_price</tool_name>\n<parameters>\n<symbol>GM</symbol>\n</parameters>\n</invoke>\n</function_calls>"
 
     println!("Result:\n{}", response);
 
@@ -122,9 +167,25 @@ Here are the tools available:
     let function_calls = response
         .content
         .exclude_function_calls()?;
+    // NOTE: An example of function results is as follows:
+    // <function_calls>
+    //   <invoke>
+    //     <tool_name>get_current_stock_price</tool_name>
+    //     <parameters>
+    //       <symbol>GM</symbol>
+    //     </parameters>
+    //   </invoke>
+    // </function_calls>
 
     // 10. Call the function.
     let function_result = tool_list.call(function_calls)?;
+    // NOTE: An example of function results is as follows:
+    // <function_results>
+    //   <result>
+    //     <tool_name>get_current_stock_price</tool_name>
+    //     <stdout>38.50</stdout>
+    //   </result>
+    // </function_results>
 
     // 11. Stack a function call by assistant.
     request_body
---
+++ src/attributes.rs
@@ -0,0 +1,4 @@
+//! Provides attributes macros from the [clust_macros](https://github.com/mochi-neko/clust/tree/main/clust_macros) crate for the `macros` feature flag.
+
+#[cfg(feature = "macros")]
+pub use clust_macros::clust_tool;
